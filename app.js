6ac045dbc82536a780e22623637c1676b26232f0
Anshumani Sharma <anshumani2001@gmail.com>
2021-10-24 13:11:53 +0530

    route changes

--- app.js
+++ app.js
@@ -14,21 +14,36 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.get('/post', (req, res) => {          //GET Requests
-    res.send('This is Post Page for Get request');
-})
-app.post('/post', (req, res) => {  //Post Req
-    res.send("This the Posts page for Post request");
-})
-app.put('/post', (req, res) => { //EDIT
-    res.send("For Making Edit Post  Request");
-})
-app.delete('/post', (req, res) => { //DELETE
-    res.send("For Deleting Posts");
-})
+
 app.get('/', (req, res) => {
+    res.send('home')
+});
+
+app.get('/posts', (req, res) => {
     res.render('posts/index')
 });
+
+app.get('/posts/new', (req, res) => {
+    res.send('To render new post form');
+})
+
+app.get('/posts/:id', async (req, res,) => {
+    res.send('to show a particular post');
+});
+
+app.get('/posts/:id/edit', async (req, res) => {
+    res.send('edit form');
+})
+
+app.post('/posts', (req, res) => {  //Post Req
+    res.send('Post request after creating a <post>');
+})
+app.put('/posts', (req, res) => { //EDIT
+    res.send("For Making Edit Post  Request");
+})
+app.delete('/posts', (req, res) => { //DELETE
+    res.send("For Deleting Posts");
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
