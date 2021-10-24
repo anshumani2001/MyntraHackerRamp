324ae84c2418d99eba6c3c8c59ebc8c398c413b7
aryan <kaintharyan@gmail.com>
2021-10-24 12:56:55 +0530

    added

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
+app.get('/post', (req, res) => {          //GET Requests
+    res.send('This is Post Page for Get request');
+})
+app.post('/post', (req, res) => {  //Post Req
+    res.send("This the Posts page for Post request");
+})
+app.put('/post', (req, res) => { //EDIT
+    res.send("For Making Edit Post  Request");
+})
+app.delete('/post', (req, res) => { //DELETE
+    res.send("For Deleting Posts");
+})
 app.get('/', (req, res) => {
     res.send('home')
 });
