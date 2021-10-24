906d38aed83e50bd467bc3bd424fc2a577f0451e
Anshumani Sharma <anshumani2001@gmail.com>
2021-10-24 12:01:51 +0530

    created basic express app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const methodOverride = require('method-override');
+
+const app=express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.send('home')
+});
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
