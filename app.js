4caa2df48be8bbf8893fac3c3edf40d3b52d3249
Anshumani Sharma <anshumani2001@gmail.com>
2021-10-24 12:59:02 +0530

    added boilerplate and layouts

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
+const ejsMate = require('ejs-mate'); 
+
 
 const app=express();
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
@@ -13,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-    res.send('home')
+    res.render('posts/index')
 });
 
 app.listen(3000, () => {
