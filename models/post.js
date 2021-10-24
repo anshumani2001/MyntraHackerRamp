a753c780669e0d3a27a9ccad8ded6335ce550fc9
Chetan <shahrachetan@gmail.com>
2021-10-24 14:22:05 +0530

    added post model

---
+++ models/post.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const comment = require('./comment')
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const opts = { toJSON: { virtuals: true } };
+
+const PostSchema = new Schema({
+    images: [ImageSchema],
+    description: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    postedAt: {
+        type: Date,
+        default: Date.now()
+    },
+    likes: Number,
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
+}, opts);
+
+
+PostSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
+});
+
+
+
+PostSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await comment.deleteMany({
+            _id: {
+                $in: doc.comments
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Campground', PostSchema);
