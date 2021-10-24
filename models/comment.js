2c9dfc3c34d6a62ca4a675a103a954dcd3444ff4
Chetan <shahrachetan@gmail.com>
2021-10-24 14:22:20 +0530

    added comment model

--- models/comment.js
+++ models/comment.js
@@ -1,1 +1,18 @@
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    body: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    postedAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model("comment", commentSchema);
+
