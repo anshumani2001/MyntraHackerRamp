aabae601f8e335625a1b888f0c59c67d8a30f37d
Chetan <shahrachetan@gmail.com>
2021-10-24 14:04:30 +0530

    added user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, firstname: {
+        type: String,
+        required: true
+    },
+    secondname: {
+        type: String,
+        required: true
+    },age: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    gender: {
+        type: String,
+        enum: ['M', 'F', 'O']
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
