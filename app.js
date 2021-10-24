9114d343ccd6828bbc146c8c50d308a88970fbca
Anshumani Sharma <anshumani2001@gmail.com>
2021-10-24 12:14:10 +0530

    added error handlers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
+const ExpressError = require('./utils/ExpressError');
 
 const app=express();
 
