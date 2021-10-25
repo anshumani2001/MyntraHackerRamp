1d239e1f228f931f3bff2d46c29ebc914e0ddaa3
Anshumani Sharma <anshumani2001@gmail.com>
2021-10-25 19:57:14 +0530

    added logout, currentUser and returnTo

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const Post = require('./models/post');
 const Comment = require('./models/comment');
 const methodOverride = require('method-override');
 const session = require('express-session');
-const flash=require('connect-flash');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const ejsMate = require('ejs-mate');
 const User = require('./models/user');
+const { isLoggedIn } = require('./middleware');
 
 const app = express();
 
@@ -53,11 +54,13 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-    res.locals.success=req.flash('success');
-    res.locals.error=req.flash('error');
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     next();
 })
+
 app.get('/', (req, res) => {
     res.render('home')
 });
@@ -67,7 +70,7 @@ app.get('/posts', async (req, res) => {
     res.render('posts/index', { posts });
 });
 
-app.get('/posts/new', (req, res) => {
+app.get('/posts/new', isLoggedIn, (req, res) => {
     res.render('posts/new')
 })
 
@@ -77,7 +80,7 @@ app.post('/posts', async (req, res) => {  //Post Req
     const newPost = new Post({ description, images });
     console.log(newPost);
     await newPost.save();
-    req.flash('success','Posted the post');
+    req.flash('success', 'Posted the post');
     res.redirect('/posts')
     // res.send(req.body);
     // res.send('Post request after creating a <post>');
@@ -88,7 +91,7 @@ app.get('/posts/:id', async (req, res,) => {
     res.render('posts/show', { post });
 });
 
-app.get('/posts/:id/edit', async (req, res) => {
+app.get('/posts/:id/edit', isLoggedIn, async (req, res) => {
     const post = await Post.findById(req.params.id);
     res.render('posts/edit', { post });
 })
@@ -98,7 +101,7 @@ app.put('/posts/:id', async (req, res) => { //EDIT
     console.log(req.body);
     const post = await Post.findByIdAndUpdate(id, { description: req.body.description });
     await post.save();
-    req.flash('success',"Post Edited")
+    req.flash('success', "Post Edited")
     res.redirect('/posts/' + post._id);
 
 })
@@ -110,22 +113,24 @@ app.post('/posts/:id/comments', async (req, res) => {
     post.comments.push(comm);
     await comm.save()
     await post.save();
-    req.flash('success','Comment Added')
+    req.flash('success', 'Comment Added')
     res.redirect('/posts/' + post.id);
 })
+
 app.delete('/posts/:id/comments/:commentid', async (req, res) => {
     const { id, commentid } = req.params;
     await Post.findByIdAndUpdate(id, { $pull: { comments: commentid } });
     await Comment.findByIdAndDelete(req.params.commentid);
-    req.flash('success',"Comment Deleted");
+    req.flash('success', "Comment Deleted");
     res.redirect('/posts/' + id);
 
 
 })
+
 app.delete('/posts/:id', async (req, res) => { //DELETE
     const { id } = req.params;
     await Post.findByIdAndDelete(id);
-    req.flash('success','Post was deleted');
+    req.flash('success', 'Post was deleted');
     res.redirect('/posts');
 })
 
@@ -133,14 +138,14 @@ app.get('/register', (req, res) => {
     res.render('Users/register.ejs');
 })
 
-app.post('/register', async(req, res) => {
+app.post('/register', async (req, res, next) => {
     try {
         const { firstname, lastname, email, username, password, age, gender } = req.body;
-        const user = new User({firstname, secondname: lastname, email, username, age, gender});
+        const user = new User({ firstname, secondname: lastname, email, username, age, gender });
         const registeredUser = await User.register(user, password);
         req.login(registeredUser, err => {
             if (err) return next(err);
-         req.flash('success', 'Successfully registered!');
+            req.flash('success', 'Successfully registered!');
             return res.redirect('/');
         })
     } catch (e) {
@@ -149,18 +154,26 @@ app.post('/register', async(req, res) => {
     }
     // res.send(req.body);
 })
+
 app.get('/login', (req, res) => {
     res.render('Users/login.ejs');
 })
 
 app.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
     console.log(req.user)
-    // const redirectUrl = req.session.returnTo || '/';
-    // req.flash('success', '####Login Message####');
-    // res.redirect(redirectUrl);
-    req.flash('success','Welcome Back');
-    res.redirect('/')
+    const redirectUrl = req.session.returnTo || '/posts';
+    delete req.session.returnTo;
+    req.flash('success', 'Welcome Back');
+    res.redirect(redirectUrl);
+    
 })
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/posts');
+})
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
